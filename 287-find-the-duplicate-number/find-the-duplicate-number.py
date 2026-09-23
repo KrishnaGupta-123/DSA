5a6bc841b8675c4be2e8cc2c90beba74e7020946
Krishna Gupta <krishna.hg@somaiya.edu>
2026-09-23 22:43:14 +0530

    Time: 28 ms (44.59%) | Memory: 33.2 MB (98.37%) - LeetSync

--- 287-find-the-duplicate-number/find-the-duplicate-number.py
+++ 287-find-the-duplicate-number/find-the-duplicate-number.py
@@ -1,21 +1,7 @@
 class Solution:
-    def BS(self, nums: list[int], i:int, j:int, target:int) -> int:
-        if i>j:
-            return -1
-        m = (i+j) // 2
-        if nums[m] == target:
-            return m
-        elif nums[m] < target:
-            return self.BS(nums,m+1,j,target)
-        else:
-            return self.BS(nums,i,m-1,target)
-
-
     def findDuplicate(self, nums: list[int]) -> int:
-        nums.sort()
-        m = len(nums)
-        for i in range(m-1):
-            idx = self.BS(nums,i + 1,m - 1,nums[i])
-            if idx != -1:
-                return nums[idx]
-                
+        for num in nums:
+            idx = abs(num) - 1
+            if nums[idx] < 0:
+                return abs(num)
+            nums[idx] *= -1
