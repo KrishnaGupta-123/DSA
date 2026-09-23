7e175418975fd96f6ec407c7ca7e40c6653f0bc6
KrishnaGupta-123 <krishna.hg@somaiya.edu>
2026-09-23 07:20:00 +0530

    Time: 0 ms (100.00%) | Memory: 19.2 MB (79.60%) - LeetSync

--- 2-add-two-numbers/add-two-numbers.py
+++ 2-add-two-numbers/add-two-numbers.py
@@ -5,22 +5,40 @@
 #         self.next = next
 class Solution:
     def addTwoNumbers(self, l1: ListNode | None, l2: ListNode | None) -> ListNode | None:
+        start_node = ListNode()
+        prev = start_node
+        
         carry = 0
-        head = None
-        cur = None
-        while l1 or l2 or carry:
-            v1 = l1.val if l1 else 0
-            v2 = l2.val if l2 else 0
-
-            val = v1 + v2 + carry
-            carry = val // 10
-            val = val % 10
-            if not head:
-                head = ListNode(val)
-                cur = head
+        while l1 or l2:
+            if not l1:
+                l1_val = 0
+                
             else:
-                cur.next = ListNode(val)
-                cur = cur.next
-            l1 = l1.next if l1 else None
-            l2 = l2.next if l2 else None
-        return head
+                l1_val = l1.val
+                l1 = l1.next
+            if not l2:
+                l2_val = 0
+            else:
+                l2_val = l2.val
+                l2 = l2.next
+                
+                
+            sum = l1_val + l2_val
+            if carry:
+                sum += 1
+                carry = 0
+                
+            if sum >= 10:
+                sum -= 10
+                carry = 1
+
+            
+            new_node = ListNode(sum)
+            prev.next = new_node
+            prev = new_node
+            
+        if carry:
+            prev.next = ListNode(1)
+        
+            
+        return start_node.next
